f031f02ff7ce6b413766fb8863bf692face4d2ab
hjy <523244758@qq.com>
2017-09-27 20:14:58 +0800

    one

---
+++ app/src/main/java/hjy/gouwu/Fragment/WeiTao.java
@@ -0,0 +1,26 @@
+package hjy.gouwu.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hjy.gouwu.R;
+
+/**
+ * Created by 胡靖宇 on 2017/9/27.
+ */
+
+public class WeiTao extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        //加载布局
+        View view=inflater.inflate(R.layout.weitao, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/hjy/gouwu/Fragment/my.java
@@ -0,0 +1,25 @@
+package hjy.gouwu.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hjy.gouwu.R;
+
+/**
+ * Created by 胡靖宇 on 2017/9/27.
+ */
+
+public class my extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        //加载布局
+        View view=inflater.inflate(R.layout.my, container, false);
+
+        return view;
+    }
+}
