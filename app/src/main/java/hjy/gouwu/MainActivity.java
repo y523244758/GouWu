17274f21877db8e9daf41ae1af035aefb0989015
hjy <523244758@qq.com>
2017-09-27 21:06:01 +0800

    09_27_viewpage_Ok

--- app/src/main/java/hjy/gouwu/MainActivity.java
+++ app/src/main/java/hjy/gouwu/MainActivity.java
@@ -6,8 +6,14 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import hjy.gouwu.Fragment.Home;
+import hjy.gouwu.Fragment.Shopping;
+import hjy.gouwu.Fragment.WeiTao;
+import hjy.gouwu.Fragment.message;
 import hjy.gouwu.Fragment.my;
 
 import static hjy.gouwu.R.id.home;
@@ -16,24 +22,39 @@ public class MainActivity extends FragmentActivity {
 
     ViewPager vp;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         vp= (ViewPager) findViewById(R.id.vp);
 
         vp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
-                Fragment fragment;
+                Fragment fragment = null;
 
                 switch (position){
                     case 0:
-
+                        fragment=new Home();
                         break;
+                    case 1:
+                        fragment=new WeiTao();
+                        break;
+                    case 2:
+                        fragment=new message();
+                        break;
+                    case 3:
+                        fragment=new Shopping();
+                        break;
+                    case 4:
+                        fragment=new my();
+                        break;
+
                 }
-                return null;
+                return fragment;
             }
 
             @Override
@@ -41,6 +62,29 @@ public class MainActivity extends FragmentActivity {
                 return 5;
             }
         });
+
+    }
+
+    public void bt(View view){
+
+        switch (view.getId()){
+            case R.id.home_ll:
+                vp.setCurrentItem(0);
+                break;
+            case R.id.weitao:
+                vp.setCurrentItem(1);
+                break;
+            case R.id.message:
+                vp.setCurrentItem(2);
+                break;
+            case R.id.shopping:
+                vp.setCurrentItem(3);
+                break;
+            case R.id.my:
+                vp.setCurrentItem(4);
+                break;
+
+        }
 
     }
 }
